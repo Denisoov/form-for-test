71940c6e461843df7e95973eb080c9c2e07a3827
Nikita Denisov <denisoov07@gmail.com>
2021-10-24 19:40:14 +0500

    Верстка шапки сайта

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-app-bar
+      app
+      elevation="0"
+      color="#F7F6FF"
+    >
+      <v-spacer></v-spacer>
+      <div class="menu">
+        <router-link class="link" to="/">Главная</router-link>
+        <router-link class="link" to="/2">Войти</router-link>
+        <router-link class="link" to="/3">Регистрация</router-link>
+      </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@mixin flex-container {
+  display: flex;
+  align-items: center;
+}
+.menu {
+  @include flex-container;
+  justify-content: space-between;
+  font-family: 'Montserrat-Medium', 'sans-serif';
+  font-size: 18px;
+  .link {
+    text-decoration: none;
+    margin-right: 50px;
+    color: #727272;
+  }
+
+  .router-link-exact-active {
+    color: #FF0036;
+  }
+
+}
+</style>
